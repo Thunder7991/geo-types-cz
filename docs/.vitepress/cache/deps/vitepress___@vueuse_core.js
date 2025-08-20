1a303cf71f255ea0c6d6859030dc0a58a2ac7512
thunderchen <853524319@qq.com>
2025-08-20 10:45:28 +0800

    build(docs): 更新文档构建产物和依赖配置
    更新了文档构建产物，包括静态资源、样式和脚本文件。调整了package.json中的vitepress版本至2.0.0-alpha.11。优化了文档构建流程，修复了部分依赖问题。

--- docs/.vitepress/cache/deps/vitepress___@vueuse_core.js
+++ docs/.vitepress/cache/deps/vitepress___@vueuse_core.js
@@ -45,6 +45,8 @@ import {
   extendRef,
   formatDate,
   formatTimeAgo,
+  formatTimeAgoIntl,
+  formatTimeAgoIntlParts,
   get,
   getLifeCycleTarget,
   getSSRHandler,
@@ -246,6 +248,7 @@ import {
   useThrottleFn,
   useThrottledRefHistory,
   useTimeAgo,
+  useTimeAgoIntl,
   useTimeout,
   useTimeoutFn,
   useTimeoutPoll,
@@ -281,7 +284,7 @@ import {
   watchTriggerable,
   watchWithFilter,
   whenever
-} from "./chunk-3GYA4YLH.js";
+} from "./chunk-PHEVA25E.js";
 import "./chunk-DDXJJ377.js";
 export {
   DefaultMagicKeysAliasMap,
@@ -337,6 +340,8 @@ export {
   extendRef,
   formatDate,
   formatTimeAgo,
+  formatTimeAgoIntl,
+  formatTimeAgoIntlParts,
   get,
   getLifeCycleTarget,
   getSSRHandler,
@@ -544,6 +549,7 @@ export {
   useThrottleFn,
   useThrottledRefHistory,
   useTimeAgo,
+  useTimeAgoIntl,
   useTimeout,
   useTimeoutFn,
   useTimeoutPoll,
@@ -580,4 +586,3 @@ export {
   watchWithFilter,
   whenever
 };
-//# sourceMappingURL=vitepress___@vueuse_core.js.map
